ff25c49bfb4e97b73831d8b132888880295fc366
etekoo <zen.taf.0210@gmail.com>
2024-06-04 08:06:41 +0000

    fix: routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,76 +1,36 @@
 Rails.application.routes.draw do
-
   # 顧客用
-# URL /customers/sign_in ...
-devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 
-namespace :admin do
-  get 'communities/index'
-  get 'communities/show'
-  get 'communities/destroy'
-end
-namespace :admin do
-  get 'plant_diaries/index'
-  get 'plant_diaries/show'
-  get 'plant_diaries/destroy'
-end
-namespace :admin do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-end
-namespace :public do
-  get 'communities/new'
-  get 'communities/index'
-  get 'communities/show'
-  get 'communities/create'
-  get 'communities/edit'
-  get 'communities/update'
-  get 'communities/destroy'
-end
-namespace :public do
-  get 'communitymembers/create'
-  get 'communitymembers/destroy'
-end
+  root 'public/homes#top' # TOPページ
 
-namespace :public do
-  get 'comments/create'
-  get 'comments/destroy'
-end
+  # その他のルーティングは以下のように整理します。
 
-namespace :public do
-  get 'plant_diaries/new'
-  get 'plant_diaries/index'
-  get 'plant_diaries/show'
-  get 'plant_diaries/create'
-  get 'plant_diaries/edit'
-  get 'plant_diaries/update'
-  get 'plant_diaries/destroy'
-end
+  scope module: :public do
+    get 'homes/about' # aboutページ
+    resources :communities do
+      resources :communitymembers, only: [:create, :destroy] # コミュニティメンバー関連
+    end
+    resources :comments, only: [:create, :destroy] # コメント関連
+    resources :plant_diaries # プラントダイアリー関連
+    resources :users, only: [:show, :edit, :update] do
+      get 'mypage', on: :collection
+      get 'unsubscribe', on: :member
+      delete 'withdraw', on: :member
+    end
+  end
 
-namespace :public do
-  get 'users/mypage'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/update'
-  get 'users/unsubscribe'
-  get 'users/withdraw'
-end
-
-namespace :public do
-  get 'homes/top'
-  get 'homes/about'
-end
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  namespace :admin do
+    resources :communities, only: [:index, :show, :destroy] # 管理者用コミュニティ関連
+    resources :plant_diaries, only: [:index, :show, :destroy] # 管理者用プラントダイアリー関連
+    resources :users, only: [:index, :show, :edit, :update] # 管理者用ユーザー関連
+  end
 end
