a222ae32ab39654c19c9b3f92767bd481cecb87d
etekoo <zen.taf.0210@gmail.com>
2024-06-13 06:50:46 +0000

    Fixes #21: コミュニティ機能を実装

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -1,10 +1,21 @@
 class Admin::CommunitiesController < ApplicationController
+  before_action :authenticate_admin! # 管理者認証
+
   def index
+    @communities = Community.all
   end
 
   def show
+    @community = Community.find(params[:id])
   end
 
   def destroy
+    @community = Community.find(params[:id])
+    @community.destroy
+    flash[:notice] = 'コミュニティを削除しました。'
+    redirect_to admin_communities_path
   end
+
+  private
+
 end
--- db/migrate/20240603192039_create_communities.rb
+++ db/migrate/20240603192039_create_communities.rb
@@ -3,7 +3,8 @@ class CreateCommunities < ActiveRecord::Migration[6.1]
     create_table :communities do |t|
 
       t.integer :user_id, null: false
-      t.string  :title, null: false
+      t.integer :owner_id
+      t.string  :name, null: false
       t.text    :introduction, null: false
       t.timestamps
     end
